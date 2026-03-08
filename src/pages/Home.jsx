6ea78de8fa66e81f149e1067458547e4fb83576f
Shruthi Kashaveni <141952842+KashaveniSruthi@users.noreply.github.com>
2026-03-08 12:10:35 +0530

    Remove import of ResultPage component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from "react";
 import "../App.css";
-import ResultPage from "../components/ResultPage";
 function HomePage() {
     const [formData, setFormData] = useState({
         companyName: "",
